0caf5ace416f4bb3b44b1a2fca40d581d3880325
kostabinov <kostabinovps@gmail.com>
2020-07-16 11:17:02 +0200

    test 6

--- understrap-child-master/header.php
+++ understrap-child-master/header.php
@@ -80,4 +80,4 @@ $container = get_theme_mod( 'understrap_container_type' );
 	</div><!-- #wrapper-navbar end -->
 
 
-<h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+<h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAasasasAAAAAAAAAAAAA</h1>
